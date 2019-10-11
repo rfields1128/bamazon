6b1de0e2c022bcecfd3c775284482f03e1ffc858
Randall <rfields1128@yahoo.com>
2019-10-10 20:15:18 -0500

    connections established and succesfully connected to mysql database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,37 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer")
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Kaplan@21!",
+    database:  "bamazon"
+});
+
+connection.connect(function(err){
+    if(err){
+        console.log("error connecting to db")
+        console.log(err);
+        return;
+    }
+    console.log("connected as id " + connection.threadId + "\n");
+    
+})
+
+connection.query("SELECT * FROM products", function(err, res){
+    if(err) throw err;
+    for (var i = 0; i < res.length; i++){
+        var itemid = res[i].item_id; 
+        var productname = res[i].product_name;
+        var departmentname = res[i].department_name;
+        var price = res[i].price;
+        var stock = res[i].stock_quantity;
+        console.log(itemid);
+        console.log(productname);
+        console.log(departmentname);
+        console.log(price);
+        console.log(stock);
+    }
+})
 
